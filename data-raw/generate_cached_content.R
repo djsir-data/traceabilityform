9141fedfefa8831f47537348af5702f60646da08
Henry McMillan <henry.mcmillan@ecodev.vic.gov.au>
2022-09-01 17:09:41 +1000

    new data-raw/app-cache setup

---
+++ data-raw/generate_cached_content.R
@@ -0,0 +1,10 @@
+
+
+# Get 10y gov bond rate
+gov_bond <- readrba::read_rba_seriesid("FCMYGBAG10D")
+gov_bond <- gov_bond$value[gov_bond$date == max(gov_bond$date)]
+saveRDS(gov_bond, "app-cache/gov_bond.rds")
+
+# Input metadata
+inmeta <- data.table::fread("data-raw/input_metadata.csv")
+saveRDS(inmeta, "app-cache/input_metadata.rds")
